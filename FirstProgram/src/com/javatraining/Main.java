4991f10ef280ca8b5f97cab400740d64fa89c980
Java <Java@LAPTOP-T7FAG709>
2023-02-14 20:53:10 +0530

    new rep

---
+++ FirstProgram/src/com/javatraining/Main.java
@@ -0,0 +1,13 @@
+package com.javatraining;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,c;
+		c=a*5;
+		c=a+5;
+		System.out.println(c);
+	}
+
+}
